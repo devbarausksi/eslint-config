fd5926189fef15b72f1b69b95809cf3fc5349f3c
Bruno de Araújo Alves <b.araujo@kingspanisoeste.com.br>
2023-11-08 09:02:34 -0300

    feat: create eslint rules

---
+++ index.js
@@ -0,0 +1,96 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    extends: [
+     "next/core-web-vitals",
+     "plugin:react/recommended",
+     "plugin:react-hooks/recommended",
+     "plugin:prettier/recommended",
+     "plugin:tailwindcss/recommended",
+     "plugin:@typescript-eslint/recommended",
+   ],
+   parser: "@typescript-eslint/parser",
+   plugins: [
+     "react",
+     "jsx-a11y",
+     "react-hooks",
+     "prettier",
+     "tailwindcss",
+     "@typescript-eslint"
+   ],
+   rules: {
+     "prettier/prettier": [
+       "error",
+       {
+         "endOfLine": "lf",
+         "printWidth": 80,
+         "tabWidth": 2,
+         "singleQuote": true,
+         "trailingComma": "all",
+         "arrowParens": "always",
+         "semi": false,
+         "singleAttributePerLine": true,
+         "plugins": [
+           "@ianvs/prettier-plugin-sort-imports"
+         ],
+         "importOrder": [
+           "^(react/(.*)$)|^(react$)",
+           "^(next/(.*)$)|^(next$)",
+           "<THIRD_PARTY_MODULES>",
+           "",
+           "^types$",
+           "^@/(.*)$",
+           "",
+           "^[./]"
+         ],
+         "importOrderParserPlugins": [
+           "typescript",
+           "jsx",
+           "decorators-legacy"
+         ]
+       }
+     ],
+     'no-unused-vars': 'off',
+     'no-empty-pattern': 'warn',
+     'no-console': 'error',
+     "react/jsx-sort-props": "error",
+     "react/jsx-curly-brace-presence": "error",
+     "react/react-in-jsx-scope": "off",
+     "react/prop-types": "off",
+     'react-hooks/exhaustive-deps': 'off',
+     "jsx-a11y/alt-text": [
+       "warn",
+       {
+         "elements": ["img"],
+         "img": ["Image"],
+       },
+     ],
+     "jsx-a11y/aria-props": "warn",
+     "jsx-a11y/aria-proptypes": "warn",
+     "jsx-a11y/aria-unsupported-elements": "warn",
+     "jsx-a11y/role-has-required-aria-props": "warn",
+     "jsx-a11y/role-supports-aria-props": "warn",
+     '@typescript-eslint/no-unused-vars': ["error", {"argsIgnorePattern": "^_"}],
+     "@typescript-eslint/consistent-type-imports": [
+       "warn",
+       {
+         "prefer": "type-imports",
+         "fixStyle": "inline-type-imports",
+       },
+     ],
+     "@typescript-eslint/no-misused-promises": [
+       2,
+       {
+         "checksVoidReturn": { "attributes": false },
+       },
+     ],
+   },
+   settings: {
+     react: {
+       version: "detect",
+     },
+     "import/parsers": {
+       [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+     },
+   },
+ };
+ 
